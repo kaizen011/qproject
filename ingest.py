f399dc2b0139bbfef5ad0b64109e706ab4d89f13
Elion Telos <125158784+kaizen011@users.noreply.github.com>
2024-04-19 15:14:17 +0200

    Added sample pdf, and ingest python file for proccessing document

---
+++ ingest.py
@@ -0,0 +1,38 @@
+from langchain.vectorstores import Qdrant
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+loader = PyPDFLoader("sample.pdf")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap = 50
+)
+texts = text_splitter.split_documents(documents)
+
+#load the embedding model
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device':'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name = model_name,
+    model_kwargs = model_kwargs,
+    encode_kwargs = encode_kwargs
+)
+
+print("Embedding Model Loaded.........")
+
+url = "http://localhost:6333"
+collection_name = "gpt_db"
+
+qdrant = Qdrant.from_documents(
+    texts,
+    embeddings,
+    url = url,
+    prefer_grpc = False,
+    collection_name = collection_name
+)
+
+print("Qdrant Index Created.......")
